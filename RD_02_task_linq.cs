7d1cf65f72915e9a113eeef68e234e664f42a41f
denis manstone <ledar54@gmail.com>
2020-04-12 22:58:47 +0300

    Добавьте файлы проекта.

---
+++ RD_02_task_linq.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApplication8
+{
+    class Film
+    {
+        public string Name { get; set; }
+        public int Year { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
